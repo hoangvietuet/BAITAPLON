9cb92eff174c0ff0579ec9ca8a0aa0aaecb99d44
Nguyễn Hoàng Việt <ln0367338@gmail.com>
2025-05-07 04:45:10 +0700

    Delete src directory

--- src/Core/Properties.cpp
+++
@@ -1,7 +0,0 @@
-#include "Properties.h"
-
-// Constructor của lớp Properties
-Properties::Properties(Vector2D pos, const std::string& texture)
-    : position(pos), textureID(texture) {
-    // Không có gì đặc biệt để làm ở đây, chỉ khởi tạo các thuộc tính từ tham số đầu vào
-}
